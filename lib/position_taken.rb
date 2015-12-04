54e6519489dda6cbb99718b428c543db5f077116
adriandave13 <adrian@paradoxlogic.net>
2015-12-03 23:30:17 -0500

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,11 @@
 # code your #position_taken? method here!
+def position_taken?(board, position)
+  if board[position.to_i] == " " || board[position.to_i] == "" || board[position.to_i] == nil
+    return false
+  end
+
+  if board[position.to_i] == "X" || board[position.to_i] == "O"
+    return true
+  end
+
+end
